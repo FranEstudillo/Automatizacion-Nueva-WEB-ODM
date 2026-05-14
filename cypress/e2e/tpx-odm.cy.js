25d5b81fa024bafa9ce260e8ee76c430e20010d2
Francisco Estudillo <franciscoestudillo97@outlook.com>
2026-05-14 11:02:29 -0600

    V1.1 Flujo de venta de un pasajero en viaje sencillo completo

--- cypress/e2e/tpx-odm.cy.js
+++ cypress/e2e/tpx-odm.cy.js
@@ -1,21 +1,25 @@
-describe("Nueva Página WEB ODM - TPX", () => {
+describe("Comité Nueva Página WEB ODM - TPX", () => {
   beforeEach(() => {
     // Ignorar errores no capturados de la app para que no fallen los tests
     Cypress.on("uncaught:exception", () => false);
     cy.visit("https://venta-dev.odm.com.mx/home");
   });
+  let origen1 = "MEXICO CENTRAL NORTE";
+  let origen2 = "MEXICO, D.F.";
+  let destino1 = "AGUASCALIENTES C. AUTOBUS";
+  // let diaSalidaViaje = "15";
 
-  it("Comité ODM - TPX", () => {
+  it("Venta de Viaje Sencillo - Usuario no registrado", () => {
     // Carga de la página
     cy.visit("https://venta-dev.odm.com.mx/home");
     // Espera para carga del logo
     cy.wait(4000);
     // Seleccionar origen
     cy.get("#originInput").click();
-    cy.get("p-selectitem").contains("MEXICO CENTRAL NORTE").click();
+    cy.get("p-selectitem").contains(origen2).click();
     // Seleccionar destino
     cy.get("#destinationInput").click();
-    cy.get("p-selectitem").contains("AGUASCALIENTES C. AUTOBUS").click();
+    cy.get("p-selectitem").contains(destino1).click();
     // Seleccionar fecha
     cy.get("#dateInput > input.p-datepicker-input").click();
 
@@ -23,8 +27,6 @@ describe("Nueva Página WEB ODM - TPX", () => {
       .not(".p-datepicker-other-month")
       .contains("15")
       .click();
-
-    cy.pause();
 
     // Seleccionar pasajeros
     cy.get("#passengersInput").click();
@@ -37,5 +39,64 @@ describe("Nueva Página WEB ODM - TPX", () => {
 
     // Buscar corridas
     cy.get(".odm-button").click();
+
+    // Seleccionar la primer corrida disponible
+    cy.get("button.ticket-btn").should("be.visible");
+    cy.get("button.ticket-btn").first().click();
+
+    // Seleccionar el primer asiento disponible
+    cy.wait(1000);
+    cy.get('img[src="media/seat.png"]').should("be.visible");
+    cy.get('img[src="media/seat.png"]').parent("div.col-9").first().click();
+
+    // Avanzar a la personalización del asiento
+    cy.get("button.btn").contains("Confirmar asientos").should("be.enabled");
+    cy.get("button.btn").contains("Confirmar asientos").click();
+
+    // Ingresar nombre del pasajero
+    cy.get('input[placeholder="Nombre completo"]')
+      .type("TEST PASAJERO CYPRESS")
+      .blur();
+
+    cy.wait(1000);
+
+    // Avanzar al pago
+    cy.get("button.btn").contains("Continuar").should("be.enabled");
+    cy.get("button.btn").contains("Continuar").click();
+
+    // Ingresar datos de pago
+    cy.get('input[placeholder="Nombre completo"]').type(
+      "TEST PASAJERO CYPRESS",
+    );
+
+    cy.get('input[placeholder="0000 0000 0000 0000"]')
+      .type("4000 0000 0000 0051")
+      .blur();
+
+    cy.get('input[placeholder="MM/AA"]').type("0139");
+
+    cy.get('input[placeholder="CVC/CVV"]').type("123");
+
+    cy.get('input[placeholder="Escribe tu nombre"]').type(
+      "TEST PASAJERO CYPRESS",
+    );
+
+    cy.get('input[placeholder="10 dígitos"]').type("5566332211");
+
+    cy.get("input#userEmail").type("jestudillo@odm.com.mx");
+
+    cy.get("input#confirmedEmail").type("jestudillo@odm.com.mx");
+
+    cy.get("input#checkPrivacy").check();
+
+    cy.get("input#checkDefault").check();
+
+    cy.wait(1000);
+
+    // Avanzar al pago
+    cy.get("button.btn").contains("Continuar").should("be.enabled");
+    cy.get("button.btn").contains("Continuar").click();
+
+    // cy.pause();
   });
 });
